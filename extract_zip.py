74f5a0b6f8aa1195fa7ec7058da9b5510841c849
“lincgcg <chunganglin@gmail.com>
2023-10-11 16:35:33 +0800

    debug

--- extract_zip.py
+++ extract_zip.py
@@ -7,6 +7,8 @@ DS_ZIP_FILENMAE = Path('ucdavis_quic.zip')
 DS_PATH = DATA_FOLDER_PATH / DS_ZIP_FILENMAE.stem
 DS_ZIP_PATH = DATA_FOLDER_PATH / DS_ZIP_FILENMAE
 
+print(DS_ZIP_PATH)
+
 logging.info(f"Extracting {DS_ZIP_PATH} to {DATA_FOLDER_PATH}")
 if not DS_PATH.exists():
     with zipfile.ZipFile(DS_ZIP_PATH, 'r') as unzip_obj:
